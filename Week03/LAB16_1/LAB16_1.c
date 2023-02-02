e38bef401cc99ef6da4f8b554df7672d53763986
Wish Kim <mang3858@daum.net>
2023-02-03 01:33:00 +0900

    update

--- Week03/LAB16_1/LAB16_1.c
+++ Week03/LAB16_1/LAB16_1.c
@@ -1,1 +1,36 @@
-ffcc
+#include <stdio.h>
+int main(void)
+{
+	int i, j, total = 0;
+	int table[5][3] = { 0 };
+
+	printf("°¡)\n");
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 3; j++)
+			printf("%d ", table[i][j]);
+		printf("\n");
+	}
+
+	for (i = 0; i < 5; i++) 
+		for (j = 0; j < 3; j++)
+			table[i][j] = i + j;
+
+	printf("³ª)\n");
+	i = 0, j = 0;
+	while (i < 5) {
+		while (j < 3)
+			printf("%d ", table[i][j++]);
+		printf("\n");
+		j = 0;
+		i++;
+	}
+
+	printf("´Ù)\n");
+	for (i = 0; i < 5; i++) {
+		for (j = 0; j < 3; j++)
+			total += table[i][j];
+	}
+	printf("Total is %d\n", total);
+
+	return 0;
+}
